3a744cf18248357be1ba2bb697b395579814bf1c
Ilya Pronin <studst2016@ya.ru>
2019-04-08 09:43:16 +0300

    *upd version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
 // As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
 DECLARE_COMPONENT_VERSION(
-"Rehearsal","0.3","Loop intervals, export/import CUE sheets.\n\nby Ilya Pronin, 2019. MIT License.\n\n"
+"Rehearsal","0.4","Loop intervals, export/import CUE sheets.\n\nby Ilya Pronin, 2019. MIT License.\n\n"
 "Fork me on github:\n"
 "https://github.com/ptytb/foo_rehearsal"
 );
